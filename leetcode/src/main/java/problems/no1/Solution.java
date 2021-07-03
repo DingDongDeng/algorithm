714dcf8a627782b233889fb20e215d8a94ffb5d4
DingDongDeng <etea583@naver.com>
2021-07-03 15:30:07 +0900

    불필요 코드 제거

--- leetcode/src/main/java/problems/no1/Solution.java
+++ leetcode/src/main/java/problems/no1/Solution.java
@@ -10,27 +10,12 @@ public class Solution {
 
     public int[] twoSum(int[] nums, int target) {
         List<Integer> sortedNumList = Arrays.stream(nums).boxed().sorted().collect(Collectors.toList());
-        List<Integer> filteredNumList = sortedNumList.subList(0, findStartOfNeedlessIndex(sortedNumList, target));
-        NumberSet numberSet = makeNumberSet(filteredNumList, target);
+        NumberSet numberSet = makeNumberSet(sortedNumList, target);
 
         return convertNumberSetToIntArray(
-            filteredNumList.get(numberSet.getIndex1()),
-            filteredNumList.get(numberSet.getIndex2()),
+            sortedNumList.get(numberSet.getIndex1()),
+            sortedNumList.get(numberSet.getIndex2()),
             nums);
-    }
-
-    private int findStartOfNeedlessIndex(List<Integer> sortedNumList, int target) {
-        for (int i = sortedNumList.size() - 1; i >= 0; i--) {
-            int value = sortedNumList.get(i);
-            if (value == target) {
-                return i;
-            }
-            if (sortedNumList.get(i) < target) {
-                return i + 1;
-            }
-        }
-
-        throw new NoSuchElementException("Invalid target value : " + target);
     }
 
     private Integer findRemainValueIndex(List<Integer> filteredNumList, int remainValue) {
