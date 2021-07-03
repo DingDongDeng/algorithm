75bddbda522af343d6367f31887112e66c45e689
DingDongDeng <etea583@naver.com>
2021-07-03 15:30:07 +0900

    불필요 코드 제거

--- leetcode/src/test/java/problems/no1/SolutionTest.java
+++ leetcode/src/test/java/problems/no1/SolutionTest.java
@@ -64,60 +64,6 @@ class SolutionTest {
     }
 
     @Test
-    void 리스트에서_불필요_인덱스_시작값_찾기() throws Exception {
-        List<Integer> sortedNumList = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
-        int target = 2;
-        int result = executeFindStartOfNeedlessIndex(sortedNumList, target);
-        int expected = 1;
-        assertThat(expected, is(result));
-    }
-
-    @Test
-    void 리스트에서_불필요_인덱스_시작값_찾기2___사잇값찾기() throws Exception {
-        List<Integer> sortedNumList = Arrays.asList(1, 5, 6, 7, 9, 11, 14);
-        int target = 8;
-        Integer result = executeFindStartOfNeedlessIndex(sortedNumList, target);
-        Integer expected = 4;
-        assertThat(expected, is(result));
-    }
-
-    @Test
-    void 리스트에서_불필요_인덱스_시작값_찾기3___끝전값찾기() throws Exception {
-        List<Integer> sortedNumList = Arrays.asList(1, 2, 3, 4, 5, 6, 9);
-        int target = 8;
-        int result = executeFindStartOfNeedlessIndex(sortedNumList, target);
-        Integer expected = 6;
-        assertThat(expected, is(result));
-    }
-
-    @Test
-    void 리스트에서_불필요_인덱스_시작값_찾기4___끝값찾기() throws Exception {
-        List<Integer> sortedNumList = Arrays.asList(1, 2, 3, 4, 5, 6, 9);
-        int target = 9;
-        Integer result = executeFindStartOfNeedlessIndex(sortedNumList, target);
-        Integer expected = 6;
-        assertThat(expected, is(result));
-    }
-
-    @Test
-    void 리스트에서_불필요_인덱스_시작값_찾기3___맨앞전값찾기() throws Exception {
-        List<Integer> sortedNumList = Arrays.asList(-4, -2, 3, 4, 5, 6, 9);
-        int target = -2;
-        int result = executeFindStartOfNeedlessIndex(sortedNumList, target);
-        Integer expected = 1;
-        assertThat(expected, is(result));
-    }
-
-    @Test
-    void 리스트에서_불필요_인덱스_시작값_찾기4___맨앞값찾기() throws Exception {
-        List<Integer> sortedNumList = Arrays.asList(-3, 2, 3, 4, 5, 6, 9);
-        int target = -3;
-        Integer result = executeFindStartOfNeedlessIndex(sortedNumList, target);
-        Integer expected = 0;
-        assertThat(expected, is(result));
-    }
-
-    @Test
     void 남은_값이_있는_인덱스_찾기__남은값이_양수() throws Exception {
         List<Integer> filteredNumList = Arrays.asList(-3, 2, 3, 4, 5, 6, 9);
         int remainValue = +6;
@@ -133,14 +79,6 @@ class SolutionTest {
         Integer result = executeFindRemainValueIndex(filteredNumList, remainValue);
         Integer expected = 1;
         assertThat(expected, is(result));
-    }
-
-
-    private Integer executeFindStartOfNeedlessIndex(List<Integer> sortedNumList, int target) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
-        log.info("sortedNumList = {}, target = {}", sortedNumList, target);
-        Method method = solution.getClass().getDeclaredMethod("findStartOfNeedlessIndex", List.class, int.class);
-        method.setAccessible(true);
-        return (int) method.invoke(solution, sortedNumList, target);
     }
 
     private Integer executeFindRemainValueIndex(List<Integer> filteredNumList, int remainValue) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
