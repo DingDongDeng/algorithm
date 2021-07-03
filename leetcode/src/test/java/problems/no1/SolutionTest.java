260308b6bddf7552807b4d200a1ac1313d7134cb
DingDongDeng <etea583@naver.com>
2021-07-03 12:20:42 +0900

    SolutionTest 샘플 수정 중

--- leetcode/src/test/java/problems/no1/SolutionTest.java
+++ leetcode/src/test/java/problems/no1/SolutionTest.java
@@ -1,11 +1,33 @@
 package problems.no1;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+//import static org.junit.jupiter.api.Assertions.*;
+//import static org.hamcrest.CoreMatchers.equalTo;
+//import static org.hamcrest.CoreMatchers.is;
+//import static org.hamcrest.MatcherAssert.assertThat;
+
+@Slf4j
 class SolutionTest {
 
     @Test
-    void twoSum() {
-        System.out.println("hi");
+    void 테스트1() {
+        Solution solution = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+
+
+//        assertThat(new int[]{0,1}, is(equalTo(solution.twoSum(nums, target))));
+    }
+
+    @Test
+    void 테스트2() {
+
+    }
+
+    @Test
+    void 테스트3() {
+
     }
 }
