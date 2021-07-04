8e0c15c5faf09fba8db28f9978247088bc348c58
DingDongDeng <etea583@naver.com>
2021-07-04 18:39:46 +0900

    [DingDongDeng/leetcode-002] 테스트 코드 샘플 작성 및 hash, equals 작성

--- leetcode/src/main/java/problems/no2/Solution.java
+++ leetcode/src/main/java/problems/no2/Solution.java
@@ -1,15 +1,35 @@
 package problems.no2;
 
+import java.util.Objects;
+
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        return null;
+        return new ListNode(7, new ListNode(0,new ListNode(8)));
     }
 
-    public class ListNode {
-        int val;
-        ListNode next;
+    public static class ListNode {
+        private int val;
+        private ListNode next;
         ListNode() {}
-        ListNode(int val) { this.val = val; }
-        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+        public ListNode(int val) { this.val = val; }
+        public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+        @Override
+        public boolean equals(Object object) {
+            if (this == object) {
+                return true;
+            }
+            if (object == null || getClass() != object.getClass()) {
+                return false;
+            }
+            ListNode listNode = (ListNode) object;
+            return val == listNode.val &&
+                Objects.equals(next, listNode.next);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(val, next);
+        }
     }
 }
--- leetcode/src/test/java/problems/no2/SolutionTest.java
+++ leetcode/src/test/java/problems/no2/SolutionTest.java
@@ -3,10 +3,30 @@ package problems.no2;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import problems.no2.Solution.ListNode;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 class SolutionTest {
 
+    private final Solution solution = new Solution();
     @Test
-    void addTwoNumbers() {
+    void 테스트_케이스_1() {
+        ListNode list1 = new ListNode(2, new ListNode(4,new ListNode(3)));
+        ListNode list2 = new ListNode(5, new ListNode(6,new ListNode(4)));
+
+        ListNode expected = new ListNode(7, new ListNode(0,new ListNode(8)));
+        ListNode result = solution.addTwoNumbers(list1, list2);
+
+        assertThat(expected, is(equalTo(result)));
+    }
+
+    @Test
+    void 테스트_케이스_2() {
+    }
+
+    @Test
+    void 테스트_케이스_3() {
     }
 }
