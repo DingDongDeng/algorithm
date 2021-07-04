e43ffe412b3e27ed05dd7958b4400b41eedd50dd
DingDongDeng <etea583@naver.com>
2021-07-04 17:52:13 +0900

    leetcode-002 init commit

---
+++ leetcode/src/main/java/problems/no2/Solution.java
@@ -0,0 +1,15 @@
+package problems.no2;
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return null;
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+}
---
+++ leetcode/src/test/java/problems/no2/SolutionTest.java
@@ -0,0 +1,12 @@
+package problems.no2;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SolutionTest {
+
+    @Test
+    void addTwoNumbers() {
+    }
+}
