20b0e1eef1c078001616d2ed448b304b193e4c49
DingDongDeng <etea583@naver.com>
2021-07-31 02:04:06 +0900

    [DingDongDeng/leetcode-005] 로직 작성(시간복잡도 고려 x)

--- leetcode/src/main/java/problems/no5/Solution.java
+++ leetcode/src/main/java/problems/no5/Solution.java
@@ -1,17 +1,11 @@
 package problems.no5;
 
-import problems.no5.PalindromeExtractor.ExtractorType;
+import java.util.Objects;
 
 class Solution {
 
     public String longestPalindrome(String s) {
-
-        PalindromeExtractor palindromeExtractor = new PalindromeExtractor();
-
-        String evenLengthStr = palindromeExtractor.getString(s, ExtractorType.EVEN);
-        String oddLengthStr = palindromeExtractor.getString(s, ExtractorType.ODD);
-
-        return evenLengthStr.length() > oddLengthStr.length() ? evenLengthStr : oddLengthStr;
+        return new PalindromeExtractor().getLongestString(s);
     }
 
 }
@@ -19,19 +13,33 @@ class Solution {
 
 class PalindromeExtractor {
 
-    public String getString(String s, ExtractorType extractorType) {
-        return extractorType == ExtractorType.EVEN ? getEvenLengthString(s) : getOddLengthString(s);
+    public String getLongestString(String s) {
+        String longestString = "";
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i; j < s.length(); j++) {
+                String str = s.substring(i, j + 1);
+                if (isPalindrome(str.toCharArray())) {
+                    longestString = str.length() > longestString.length() ? str : longestString;
+                }
+            }
+        }
+        return longestString;
     }
 
-    private String getEvenLengthString(String s) {
-        return "";
+    private boolean isPalindrome(char[] chars) {
+        if (Objects.isNull(chars) || chars.length == 0) {
+            return false;
+        }
+        for (int i = 0; i < chars.length; i++) {
+            if (i > chars.length / 2) {
+                break;
+            }
+            if (chars[i] != chars[chars.length - 1 - i]) {
+                return false;
+            }
+        }
+
+        return true;
     }
 
-    private String getOddLengthString(String s) {
-        return "";
-    }
-
-    public enum ExtractorType {
-        ODD, EVEN;
-    }
 }
--- leetcode/src/test/java/problems/no5/SolutionTest.java
+++ leetcode/src/test/java/problems/no5/SolutionTest.java
@@ -3,6 +3,8 @@ package problems.no5;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -10,32 +12,55 @@ import org.junit.jupiter.api.Test;
 class SolutionTest {
 
     private final Solution solution = new Solution();
+
     @Test
-    public void 기본_테스트_케이스_1(){
+    public void 기본_테스트_케이스_1() {
         String input = "babad";
         String expected = "bab";
         assertThat(expected, is(solution.longestPalindrome(input)));
     }
 
     @Test
-    public void 기본_테스트_케이스_2(){
+    public void 기본_테스트_케이스_2() {
         String input = "cbbd";
         String expected = "bb";
         assertThat(expected, is(solution.longestPalindrome(input)));
     }
 
     @Test
-    public void 기본_테스트_케이스_3(){
+    public void 기본_테스트_케이스_3() {
         String input = "a";
         String expected = "a";
         assertThat(expected, is(solution.longestPalindrome(input)));
     }
 
     @Test
-    public void 기본_테스트_케이스_4(){
+    public void 기본_테스트_케이스_4() {
         String input = "ac";
         String expected = "a";
         assertThat(expected, is(solution.longestPalindrome(input)));
     }
 
+    @Test
+    public void 회문_감별_로직_테스트() throws NoSuchMethodException, IllegalAccessException, InvocationTargetException {
+        assertThat(true, is(executeIsPalindrome("a".toCharArray())));
+        assertThat(true, is(executeIsPalindrome("aa".toCharArray())));
+        assertThat(true, is(executeIsPalindrome("aaa".toCharArray())));
+        assertThat(true, is(executeIsPalindrome("abcba".toCharArray())));
+        assertThat(true, is(executeIsPalindrome("abba".toCharArray())));
+        assertThat(false, is(executeIsPalindrome("de".toCharArray())));
+        assertThat(false, is(executeIsPalindrome("def".toCharArray())));
+        assertThat(false, is(executeIsPalindrome("gfgf".toCharArray())));
+        assertThat(false, is(executeIsPalindrome("gffgs".toCharArray())));
+        assertThat(false, is(executeIsPalindrome("dddf".toCharArray())));
+        assertThat(false, is(executeIsPalindrome("".toCharArray())));
+    }
+
+    private final PalindromeExtractor palindromeExtractor = new PalindromeExtractor();
+
+    private boolean executeIsPalindrome(char[] chars) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Method method = palindromeExtractor.getClass().getDeclaredMethod("isPalindrome", char[].class);
+        method.setAccessible(true);
+        return (Boolean) method.invoke(palindromeExtractor, chars);
+    }
 }
