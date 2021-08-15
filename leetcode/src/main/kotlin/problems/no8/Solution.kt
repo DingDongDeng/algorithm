4a6cd59de3298a9fe4b5c6f315cc76190058c2d1
DingDongDeng <etea583@naver.com>
2021-08-15 18:26:55 +0900

    [DingDongDeng/leetcode-008] init commit

---
+++ leetcode/src/main/kotlin/problems/no8/Solution.kt
@@ -0,0 +1,7 @@
+package problems.no8
+
+class Solution {
+    fun myAtoi(s: String): Int {
+       return 0;
+    }
+}
---
+++ leetcode/src/test/kotlin/problems/no8/SolutionTest.kt
@@ -0,0 +1,48 @@
+package problems.no8
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.hamcrest.CoreMatchers.`is`
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.jupiter.api.Test
+
+internal class SolutionTest {
+    private val solution: Solution = Solution()
+
+    @Test
+    fun 테스트_케이스_1() {
+        val input = "42"
+        val output = 42
+        assertThat(output, `is`(equalTo(solution.myAtoi(input))))
+    }
+
+    @Test
+    fun 테스트_케이스_2() {
+        val input = "   -42"
+        val output = -42
+        assertThat(output, `is`(equalTo(solution.myAtoi(input))))
+    }
+
+    @Test
+    fun 테스트_케이스_3() {
+        val input = "4193 with words"
+        val output = 4193
+        assertThat(output, `is`(equalTo(solution.myAtoi(input))))
+    }
+
+    @Test
+    fun 테스트_케이스_4() {
+        val input = "words and 987"
+        val output = 0
+        assertThat(output, `is`(equalTo(solution.myAtoi(input))))
+    }
+
+    @Test
+    fun 테스트_케이스_5() {
+        val input = "-91283472332"
+        val output = -2147483648
+        assertThat(output, `is`(equalTo(solution.myAtoi(input))))
+    }
+
+}
