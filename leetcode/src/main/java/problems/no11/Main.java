cb7029e234715159fe4a084525217e51261e20e6
lcw3176 <lcw3176@naver.com>
2023-01-08 17:08:55 +0900

    커밋

---
+++ leetcode/src/main/java/problems/no11/Main.java
@@ -0,0 +1,12 @@
+package problems.no11;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int[] arr = {1, 8, 6, 2, 5, 4, 8, 3, 7};
+//        int[] arr = {4, 3, 2, 1, 4};
+        int[] arr = {1, 2, 1};
+        new Solution().maxArea(arr);
+    }
+
+}
