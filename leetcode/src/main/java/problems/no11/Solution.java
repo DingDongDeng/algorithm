e46fc4ae00b104c460d36a80dc222798ed37065e
jim <jim@gccompany.co.kr>
2023-01-08 15:45:50 +0900

    [DingDongDeng/leetcode-011] 연산이 불필요한 경우는 스킵

--- leetcode/src/main/java/problems/no11/Solution.java
+++ leetcode/src/main/java/problems/no11/Solution.java
@@ -5,7 +5,11 @@ class Solution {
     public int maxArea(int[] height) {
         int maxArea = 0;
         for (int i = 0; i < height.length - 1; i++) {
-            for (int j = i + 1; j < height.length; j++) {
+            if (height[i] == 0) {
+                continue;
+            }
+            int startWidth = maxArea / height[i];
+            for (int j = startWidth; j < height.length; j++) {
                 int leftHeight = height[i];
                 int rightHeight = height[j];
                 int width = j - i;
